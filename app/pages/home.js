82476a3707ec541505931144ef9abe9b1a7bce73
Krunal Patel <krunalp@krunalp.local>
2017-03-17 10:40:06 +0530

    Krunal - removed image

--- app/pages/home.js
+++ app/pages/home.js
@@ -18,12 +18,8 @@ export default class Home extends React.Component {
 		return (
 			<div className="container home">
 				<div className="row">
-					<div className="col-lg-3">
-						<img src="http://www.theholidayspot.com/patrick/images/st-patricks-day-beer.jpg" />
-					</div>
-					<div className="col-lg-9">
-						<h2>Welcome to DEV Bootcamp !!!</h2>
-						<p>Happy St Patrick's Day !!!</p>
+					<div className="col-lg-12">
+						<h2>Happy St Patrick's Day !!!</h2>
 						<Link to="/deck">Go to twitter deck.</Link>
 					</div>
 				</div>
